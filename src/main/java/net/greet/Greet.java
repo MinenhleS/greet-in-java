949b8de8de746b4ad3642dced427635cc3e5d96c
MinenhleS <ngwenyasiyamukela@gmail.com>
2019-04-07 17:03:56 +0200

    update

--- src/main/java/net/greet/Greet.java
+++ src/main/java/net/greet/Greet.java
@@ -1,6 +1,9 @@
 package net.greet;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Greet {
 
     String name;
@@ -13,6 +16,8 @@ public class Greet {
     public Greet() {
 
     }
+
+    Map< Languages, Integer> addUserMap = new HashMap< Languages, Integer>();
 
     public void GreetM(String name, String lang) {
 
@@ -27,6 +32,30 @@ public class Greet {
 
     }
 
+    public void adding(Languages addUser){
+
+        if (!addUserMap.containsKey(addUser)) {
+            addUserMap.put(addUser, 0);
+        }
+
+        // increment the counter for this type
+
+        int userCounter = addUserMap.get(addUser);
+        userCounter++;
+        addUserMap.put(addUser, userCounter);
+    }
+
+    public int totalUsers() {
+
+        int totalAddedUser = 0;
+
+        for (Integer userCount : addUserMap.values()) {
+            totalAddedUser += userCount;
+        }
+
+        return totalAddedUser;
+
+    }
 
     public String getName() {
         return name;
@@ -39,11 +68,9 @@ public class Greet {
         try {
             return Languages.valueOf(lang).getLang();
         } catch (IllegalArgumentException e) {
-//            e.printStackTrace();
+
             return Languages.valueOf("IsiZulu").getLang();
         }
-//        return  null;
-//        return Languages.valueOf(lang).getLang();
     }
 
 
