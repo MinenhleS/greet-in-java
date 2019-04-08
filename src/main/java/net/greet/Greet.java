dd93e0996134b3b63dec28c46c5861a8779a14ea
MinenhleS <ngwenyasiyamukela@gmail.com>
2019-04-08 15:33:38 +0200

    update

--- src/main/java/net/greet/Greet.java
+++ src/main/java/net/greet/Greet.java
@@ -47,25 +47,41 @@ public class Greet {
 
     public void counter(){
 
-        System.out.println( addUserMap.size());
+        System.out.println( addUserMap.size() + "user greeted");
     }
     public void greeted(){
 
-        System.out.println( addUserMap);
+        System.out.println("The list of the greeted : " + addUserMap);
     }
 
 
-    public void greetUsers(String name) {
+    public void greetedUsers(String name) {
 
-//        if (!addUserMap.containsKey(name)) {
-//            return addUserMap;
-//        }
-//            else
-        System.out.println(addUserMap.get(name) + name + "greeted");
+        System.out.println( name + " is greeted "+ addUserMap.get(name) + " time(s)");
 
     }
 
+    public void clear() {
+        addUserMap.clear();
+    }
 
+    public void clear(String name) {
+        addUserMap.put(name, 0);
+    }
+
+    public String help(){
+
+        return "Valid commands are:\n" +
+                "\n" +
+                "greet followed by the name and the language the user is to be greeted in,\n" +
+                "greeted should display a list of all users that has been greeted and how many time each user has been greeted,\n" +
+                "greeted followed by a username returns how many times that username have been greeted,\n" +
+                "counter returns a count of how many unique users has been greeted,\n" +
+                "clear deletes of all users greeted and the reset the greet counter to 0,\n" +
+                "clear followed by a username delete the greet counter for the specified user and decrement the greet counter by 1,\n" +
+                "exit exits the application,\n" +
+                "help shows a user an overview of all possible commands.";
+    }
 
 
     public String getName() {
--- src/main/java/net/greet/RunGreet.java
+++ src/main/java/net/greet/RunGreet.java
@@ -16,9 +16,14 @@ public class RunGreet {
 
         while (true) {
 
-            System.out.println("Enter a command : ");
-            command = input.nextLine();
-            String[] word = command.trim().split("\\s");
+                System.out.println(" \n Enter a command : ");
+                command = input.nextLine();
+                String[] word = command.trim().split("\\s");
+
+            if(command.equalsIgnoreCase("exit")) {
+                System.out.println(" \n Goodbye have a nice day !!!");
+                break;
+            }
 
                 if (word[0].equalsIgnoreCase("greet")) {
 
@@ -29,46 +34,65 @@ public class RunGreet {
                         in.adding(word[1]);
 
                         System.out.println(in.getLang() + ", " + in.getName());
-
+                        System.out.println(" \n Added!");
 
                     }
+
                     else if (!(word.length == 3)) {
                         in.GreetM(word[1], null);
                         in.adding(word[1]);
 
-                        System.out.println( in.getLang() + "," + in.getName());
+                        System.out.println(in.getLang() + "," + in.getName());
+                        System.out.println(" \n Added!");
 
                     }
-
                 }
 
-                else if(word[0].equalsIgnoreCase("greeted")){
+                else if (word[0].equalsIgnoreCase("greeted")) {
+
+
                         if (!(word.length == 2)) {
-                             in.greeted();
+                                in.greeted();
                         }
 
-                        else if(word.length == 2){
+                        else if (word.length == 2) {
 
-                            in.greetUsers(word[1]);
+                             in.greetedUsers(word[1]);
                         }
                 }
 
-                else if(word[0].equalsIgnoreCase("counter")){
+
+                else if (word[0].equalsIgnoreCase("counter")) {
 
                     in.counter();
                 }
 
-            System.out.println(" \n Added!");
-            System.out.println(" \n \n ");
+                else if(word[0].equalsIgnoreCase("clear")){
 
-            String option = "";
-            System.out.println("You want to continue ??? Y / N  \n");
-            option = input.nextLine();
+                        if(!(word.length == 2)){
+                            in.clear();
+                        }
+                        else if(word.length == 2){
+                            in.clear(word[1]);
+                        }
+                }
 
-            if (!option.equalsIgnoreCase("Y")) {
-                break;
+                else if(word[0].equalsIgnoreCase("help"){
+
 
             }
+
+
+
+                System.out.println(" \n \n ");
+                String option = "";
+                System.out.println("You want to continue ??? Y / N  \n");
+                option = input.nextLine();
+
+                if (!option.equalsIgnoreCase("Y")) {
+                    break;
+
+                }
 
 
 
